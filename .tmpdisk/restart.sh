edb2e70e60ae5ca898b90363bd8b3b8bccda6e3b
chiguaxiaoer <chiguaxiaoer@gmail.com>
2026-06-18 21:45:07 +0700

    chore: remove stray .tmpdisk, add to .gitignore

--- .tmpdisk/restart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-pkill -9 -f "strapi develop" 2>/dev/null
-lsof -ti tcp:1337 2>/dev/null | xargs kill -9 2>/dev/null
-sleep 4
-cd /Users/mac/newmoban/apps/cms
-export TMPDIR=/Users/mac/newmoban/.tmpdisk
-export PATH="/usr/local/opt/node@20/bin:$PATH"
-nohup pnpm develop > /Users/mac/newmoban/.tmpdisk/cms.log 2>&1 &
-echo "launched pid $!" > /Users/mac/newmoban/.tmpdisk/restart-result.txt
